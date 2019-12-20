c738810dc71abe3a70f5d801c6830e9304814c77
ricgra <59093472+ricgra@users.noreply.github.com>
2019-12-20 17:14:35 +0100

    first commit

---
+++ converter.js
@@ -0,0 +1,17 @@
+const csvtojson = require('csvtojson')
+const path = require('path')
+const fs = require('fs')
+
+const csvPath = path.join(__dirname, 'customer-data.csv')
+const jsonOutPath = path.join(__dirname, 'customer-data.json')
+
+csvtojson()
+    .fromFile(csvPath)
+    .then((json) => {
+        fs.writeFile(jsonOutPath, JSON.stringify(json, null, 2), (error) => {
+            if (error) {
+                return console.log('Error during JSON conversion from CSV [', error, ']')
+            }
+            console.log('Conversion ended successfully')
+        })
+    })
